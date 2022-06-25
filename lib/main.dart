a86f13bb90a5ecfbaa043ac4724bc1a1a5ba5138
Eduardo Leonardo <eduardo.rosa@terceiroslinx.com.br>
2022-06-25 12:58:01 -0300

    Creating project

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: Container(
+      child: const Text('Teste'),
+      color: Colors.red,
+    ),
+  ));
+}
