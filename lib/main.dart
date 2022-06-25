762efb5b4082025da3199b6c6003d437018e657e
Eduardo Leonardo <eduardo.rosa@terceiroslinx.com.br>
2022-06-25 13:02:26 -0300

    Creating first stateless widget (Home page)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MaterialApp(
-    home: Container(
-      child: const Text('Teste'),
-      color: Colors.red,
-    ),
-  ));
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: Column(
+          children: const [
+            Text(
+              'Hello World',
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
